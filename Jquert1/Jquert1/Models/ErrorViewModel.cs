77a463d2c815642dc6c462e7832130bfdf8fd868
e4ekip <sehendsina@gmail.com>
2022-05-09 18:48:20 +0300

    Jquery

---
+++ Jquert1/Jquert1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Jquert1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
